413fed79136cc62392d8cdde43a33a712d0c4ebf
Christiana Routon <christiana.routon@gmail.com>
2018-03-05 14:28:20 -0600

    first commit

---
+++ server/routers/messageRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+
+
+module.exports = router;
